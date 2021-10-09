e8c01ae1b2f8297c09731cb7caa6a5ddb27a1937
Shelling Hsu <navyblueshellingford@gmail.com>
2021-10-09 10:33:32 -0700

    test for containing minutes in end time

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -8,4 +8,8 @@ describe WeeklyHours do
   it "can parse single day with time containing minutes in start time" do
     expect(WeeklyHours::Parser.new.parse("Mon 10:10 am - 5 pm").tokenize).must_equal [[1], [10*3600+10*60, 17*3600]]
   end
+
+  it "can parse single day with time containing minutes in end time" do
+    expect(WeeklyHours::Parser.new.parse("Mon 10 am - 5:20 pm").tokenize).must_equal [[1], [10*3600, 17*3600+20*60]]
+  end
 end
