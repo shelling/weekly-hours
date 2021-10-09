70c1f5b96393391056232eeda7edf5d69b17f403
Shelling Hsu <navyblueshellingford@gmail.com>
2021-10-08 19:33:32 -0700

    test for containing minutes in start time

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -1,7 +1,11 @@
 require_relative "test_helper"
 
 describe WeeklyHours do
-  it "it can parse single day with time" do
+  it "can parse single day with time" do
     expect(WeeklyHours::Parser.new.parse("Mon 10 am - 5 pm").tokenize).must_equal [[1], [10*3600, 17*3600]]
+  end
+
+  it "can parse single day with time containing minutes in start time" do
+    expect(WeeklyHours::Parser.new.parse("Mon 10:10 am - 5 pm").tokenize).must_equal [[1], [10*3600+10*60, 17*3600]]
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 $:.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
 require 'weekly-hours'
 require 'minitest/autorun'
+require "minitest/spec"
